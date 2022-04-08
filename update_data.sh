6387d29165d405109a25e46826d64a3c528f3396
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-08 11:37:00 +0200

    next/prev added

--- update_data.sh
+++ update_data.sh
@@ -9,3 +9,6 @@ ant -f fetch-data.xml
 echo "delete line zones"
 
 python delete_line_zones.py
+
+echo "add xml:id, prev and next attributes"
+add-attributes -g "./data/editions/*.xml" -b "https://id.acdh.oeaw.ac.at/wkfm"
